6419f2d9d0cba3cd116bd237077a14d70ef237b3
Grant Kiely <1948935+gkiely@users.noreply.github.com>
2025-08-14 10:40:56 -0700

    looks like it's fixed

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 import './style.css';
-import javascriptLogo from './javascript.svg';
 import { setupCounter } from './counter.js';
 
-import pdfjs from 'pdfjs-dist';
+import * as pdfjs from 'pdfjs-dist';
+console.log(pdfjs);
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const el = document.querySelector('#app');
+if(el){
+  el.innerHTML = `
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <h1>Hello Vite!</h1>
+      <div class="card">
+        <button id="counter" type="button"></button>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite logo to learn more
+      </p>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+  `;
+}
+
 
 setupCounter(document.querySelector('#counter'));
